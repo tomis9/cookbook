b2f000d8bdbd5d17645db754dbc2820867cac803
tomis9 <tomis9@buziaczek.pl>
2018-10-20 22:36:32 +0200

    logging: config in a dictionary

--- logging_tutorial/app.py
+++ logging_tutorial/app.py
@@ -1,16 +1,44 @@
-import logging
+import logging.config
 from import_app import func
 
 # resetting basic config set in another file
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
 
+
+config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'app': {
+            'formatter': 'standard',
+            'class': 'logging.FileHandler',
+            'filename': './logs.log',
+        },
+        'sub': {
+            'formatter': 'standard',
+            'class': 'logging.FileHandler',
+            'filename': './logs_imp.log',
+        },
+    },
+    'loggers': {
+        'app': {
+            'level': 'DEBUG',
+            'handlers': ['app'],
+        },
+        'imp': {
+            'level': 'INFO',
+            'handlers': ['sub'],
+        },
+    }
+}
+logging.config.dictConfig(config)
 logger = logging.getLogger('app')
-logger.setLevel(logging.DEBUG)
-hdl = logging.FileHandler('./logs.log')
-format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-hdl.setFormatter(logging.Formatter(format_str))
-logger.addHandler(hdl)
 
 logger.debug('debug one')
 
--- logging_tutorial/import_app.py
+++ logging_tutorial/import_app.py
@@ -1,18 +1,11 @@
 import logging
-import sys
 
 
 # purposely harmful logging declaration <begin
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 # end>
 
 logger = logging.getLogger('imp')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.FileHandler('./logs_imp.log'))
-
-stdoutHandler = logging.StreamHandler(sys.stdout)
-stdoutHandler.setLevel(logging.WARNING)
-logger.addHandler(stdoutHandler)
 
 
 def func():
