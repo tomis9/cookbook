5411573dcb039255c8d36b13730d9cd61dc6199c
tomis9 <tomis9@buziaczek.pl>
2018-10-20 22:47:12 +0200

    logging: config from a json file

--- logging_tutorial/app.py
+++ logging_tutorial/app.py
@@ -1,42 +1,14 @@
 import logging.config
+import json
 from import_app import func
 
 # resetting basic config set in another file
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
 
+with open('./config.json') as f:
+    config = json.load(f)
 
-config = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'app': {
-            'formatter': 'standard',
-            'class': 'logging.FileHandler',
-            'filename': './logs.log',
-        },
-        'sub': {
-            'formatter': 'standard',
-            'class': 'logging.FileHandler',
-            'filename': './logs_imp.log',
-        },
-    },
-    'loggers': {
-        'app': {
-            'level': 'DEBUG',
-            'handlers': ['app'],
-        },
-        'imp': {
-            'level': 'INFO',
-            'handlers': ['sub'],
-        },
-    }
-}
 logging.config.dictConfig(config)
 logger = logging.getLogger('app')
 
