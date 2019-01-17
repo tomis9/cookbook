c05dbfcb08779272b3339e08b6df04eff6c2ce82
Tomasz Dyrka <tomasz.dyrka@iiit.pl>
2019-01-17 20:26:38 +0100

    reddit: beginning of sentiment analysis

--- scratchpad/vim_vs_emacs/text_mining.R
+++ scratchpad/vim_vs_emacs/text_mining.R
@@ -49,14 +49,27 @@ bodies <- da %>%
 bodies %>%
   count(editor)
 
-library(tidytext)
-# https://www.tidytextmining.com/tidytext.html#the-unnest_tokens-function
-tokens <- unnest_tokens(bodies, "word", body)
 
-data(stop_words)
+# all the quotes/disclaimers are my comments regarding my workflow or personal opinions, not the analysis itself
+#> all the calculations above could have been performed on spark using sparklyr, but I didn't do it, as this would be a huge overkill for a dataset that small. However, if I obtain interesting results, I will move to (sparklyr)[https://spark.rstudio.com/guides/textmining/] and maybe run calclulations on AWS. For now let's use tidytext and work locally, while keeping the app pretty easy to scale (e.g. encapsulate into functions these calculations, which can be replaced with sparklyr int the nearest future).
+
+#######################################
+# a quick exploratory analysis
+tidy_frame <- function(frame, column) {
+  tokens <- tidytext::unnest_tokens_(frame, "word", column)
+
+  tokens <- tokens %>%
+    anti_join(tidytext::stop_words, by = "word")
+
+  return(tokens)
+}
+
+tokens <- tidy_frame(bodies, "body")
 
 tokens <- tokens %>%
-  anti_join(stop_words)
+  filter(!word %in% c("vim", "emacs"))
+
+# https://www.tidytextmining.com/tidytext.html#the-unnest_tokens-function
 
 tokens %>%
   count(editor, word) %>%
@@ -67,3 +80,58 @@ tokens %>%
   facet_wrap(~editor, nrow = 2) +
   coord_flip()
  
+library(ggplot2)
+# library(tidyr)
+p_t <- tokens %>%
+  count(editor, word) %>%
+  select(editor, word, n_occur = n) %>%
+  group_by(editor) %>%
+  mutate(freq = n_occur / sum(n_occur)) %>%
+  select(editor, word, freq) %>%
+  spread(editor, freq) %>%
+  ggplot(aes(x = vim, y = emacs)) +
+  geom_jitter() +
+  geom_text(aes(label = word))
+ggplotly(p_t)
+
+# you can zoom the plot easily
+
+# package vs plugin - emacs users clearly use packages, while vim users - plugins
+# vim plugins are stored on github
+# undoing seems to be the case among emacs users
+# vim commands (command mode) is still complicated and user-unfriendly
+# vim 8, which is relatively new, makes multi-threaded computations much easier
+# emacs users are more aware of documentation ;)
+
+# a dictionary of synonyms could be useful - we could cluster the words
+
+
+#######################################
+# sentiment analysis
+# ithey do, t is very difficult to tell which text editor is better, as there are very few people who know well both of them. Even if they do, they are probably very... unusual people with unusual hobbys or needs, and generalization made on observing them would be doubtful.
+# But, what we can do, is measuring programmers' satisfaction among vim and emacs users.
+
+# sentiment analysis using lstm and tensorflow
+# https://www.oreilly.com/learning/perform-sentiment-analysis-with-lstms-using-tensorflow
+
+tokens %>%
+  inner_join(tidytext::get_sentiments("afinn")) %>%
+  group_by(editor, post_num) %>%
+  summarise(mean_score = mean(score)) %>%
+  ungroup() %>%
+  ggplot(aes(x = mean_score, color = editor)) +
+  geom_density()
+
+# it seems like vim users are happier. We could still perform this analysis with sparklyr.
+  
+
+library(wordcloud)
+
+tokens %>%
+  filter(editor == "vim") %>%
+  count(word) %>%
+  with(wordcloud(word, n, max.words = 100))
+
+# wider analysis: specific words for emacs and vim comparing to atom (Atom), sublime text (SublimeText), visual studio code (vscode), Brackets (brackets), notepad++ (notepadplusplus)
+
+# what's next? tf-idf  https://www.tidytextmining.com/tfidf.html
