8cdf2a05814d25689670197d261bccc7f5fc300d
tomis9 <tomis9@buziaczek.pl>
2019-02-10 12:21:33 +0100

    tensorflow example in scratchpad

--- scratchpad/tensorflow_lm/tensorflow_lm.py
+++ scratchpad/tensorflow_lm/tensorflow_lm.py
@@ -10,7 +10,7 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy
 import matplotlib.pyplot as plt
-rng = numpy.random
+import pandas as pd
 
 # Parameters
 learning_rate = 0.01
@@ -18,10 +18,10 @@ training_epochs = 1000
 display_step = 50
 
 # Training Data
-train_X = numpy.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
-                         7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-train_Y = numpy.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
-                         2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+data = pd.read_csv('./data.csv', index_col=False)
+
+train_X = numpy.asarray(data.X)
+train_Y = numpy.asarray(data.Y)
 n_samples = train_X.shape[0]
 
 # tf Graph Input
@@ -29,8 +29,8 @@ X = tf.placeholder("float")
 Y = tf.placeholder("float")
 
 # Set model weights
-W = tf.Variable(rng.randn(), name="weight")
-b = tf.Variable(rng.randn(), name="bias")
+W = tf.Variable(numpy.random.randn(), name="weight")
+b = tf.Variable(numpy.random.randn(), name="bias")
 
 # Construct a linear model
 pred = tf.add(tf.multiply(X, W), b)
@@ -38,7 +38,8 @@ pred = tf.add(tf.multiply(X, W), b)
 # Mean squared error
 cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
 # Gradient descent
-#  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
+# Note, minimize() knows to modify W and b because Variable objects are
+# trainable=True by default
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
 # Initialize the variables (i.e. assign their default value)
@@ -57,13 +58,14 @@ with tf.Session() as sess:
 
         # Display logs per epoch step
         if (epoch+1) % display_step == 0:
-            c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
-                "W=", sess.run(W), "b=", sess.run(b))
+            c = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c),
+                  "W=", sess.run(W), "b=", sess.run(b))
 
     print("Optimization Finished!")
     training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-    print("Training cost=", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+    print("Training cost=", training_cost, "W=", sess.run(W), "b=",
+          sess.run(b), '\n')
 
     # Graphic display
     plt.plot(train_X, train_Y, 'ro', label='Original data')
