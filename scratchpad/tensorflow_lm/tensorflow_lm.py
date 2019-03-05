b7c2eade4509c4af09bf6e46935ffed55a1bf57c
tomis9 <tomis9@buziaczek.pl>
2019-03-05 19:16:21 +0100

    a few examples of tensorflow

--- scratchpad/tensorflow_lm/tensorflow_lm.py
+++ scratchpad/tensorflow_lm/tensorflow_lm.py
@@ -1,91 +1,27 @@
-'''
-A linear regression learning algorithm example using TensorFlow library.
-
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-'''
-
-from __future__ import print_function
-
 import tensorflow as tf
-import numpy
-import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+import sklearn.linear_model
 
-# Parameters
-learning_rate = 0.01
-training_epochs = 1000
-display_step = 50
+# preparing data
+housing = fetch_california_housing()
+m, n = housing.data.shape
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
 
-# Training Data
-data = pd.read_csv('./data.csv', index_col=False)
+# tensorflow way
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
 
-train_X = numpy.asarray(data.X)
-train_Y = numpy.asarray(data.Y)
-n_samples = train_X.shape[0]
-
-# tf Graph Input
-X = tf.placeholder("float")
-Y = tf.placeholder("float")
-
-# Set model weights
-W = tf.Variable(numpy.random.randn(), name="weight")
-b = tf.Variable(numpy.random.randn(), name="bias")
-
-# Construct a linear model
-pred = tf.add(tf.multiply(X, W), b)
-
-# Mean squared error
-cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
-# Gradient descent
-# Note, minimize() knows to modify W and b because Variable objects are
-# trainable=True by default
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-# Initialize the variables (i.e. assign their default value)
-init = tf.global_variables_initializer()
-
-# Start training
 with tf.Session() as sess:
+    theta_value = theta.eval()
 
-    # Run the initializer
-    sess.run(init)
+np.set_printoptions(suppress=True)
+print(theta_value.ravel())
 
-    # Fit all training data
-    for epoch in range(training_epochs):
-        for (x, y) in zip(train_X, train_Y):
-            sess.run(optimizer, feed_dict={X: x, Y: y})
-
-        # Display logs per epoch step
-        if (epoch+1) % display_step == 0:
-            c = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c),
-                  "W=", sess.run(W), "b=", sess.run(b))
-
-    print("Optimization Finished!")
-    training_cost = sess.run(cost, feed_dict={X: train_X, Y: train_Y})
-    print("Training cost=", training_cost, "W=", sess.run(W), "b=",
-          sess.run(b), '\n')
-
-    # Graphic display
-    plt.plot(train_X, train_Y, 'ro', label='Original data')
-    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    plt.legend()
-    plt.show()
-
-    # Testing example, as requested (Issue #2)
-    test_X = numpy.asarray([6.83, 4.668, 8.9, 7.91, 5.7, 8.7, 3.1, 2.1])
-    test_Y = numpy.asarray([1.84, 2.273, 3.2, 2.831, 2.92, 3.24, 1.35, 1.03])
-
-    print("Testing... (Mean square loss Comparison)")
-    testing_cost = sess.run(
-        tf.reduce_sum(tf.pow(pred - Y, 2)) / (2 * test_X.shape[0]),
-        feed_dict={X: test_X, Y: test_Y})  # same function as cost above
-    print("Testing cost=", testing_cost)
-    print("Absolute mean square loss difference:", abs(
-        training_cost - testing_cost))
-
-    plt.plot(test_X, test_Y, 'bo', label='Testing data')
-    plt.plot(train_X, sess.run(W) * train_X + sess.run(b), label='Fitted line')
-    plt.legend()
-    plt.show()
+# scikit-learn way
+ls = sklearn.linear_model.LinearRegression()
+ls.fit(housing_data_plus_bias, housing.target)
+print(ls.intercept_)
+print(ls.coef_)
