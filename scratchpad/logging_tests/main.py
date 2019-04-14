7c7f06baa07e7d6747ddebad0c4df494087ce9ab
tomis9 <tomis9@buziaczek.pl>
2019-04-14 13:07:13 +0200

    logging: custom handler

--- scratchpad/logging_tests/main.py
+++ scratchpad/logging_tests/main.py
@@ -1,18 +1,22 @@
 import logging.config
 import json
 
+
 with open('./config.json') as f:
     config = json.load(f)
 
 logging.config.dictConfig(config)
 logger = logging.getLogger('app')
 
+# hdl = CustomHandler()
+# logger.addHandler(hdl)
+
 logger.debug('first')
 
 try:
     x = 1 / 0
 except ZeroDivisionError as e:
-    logger.error(str(e))
+    logger.error(e)
     raise
 
 
