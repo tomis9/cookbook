7287f595a3ba0792f530d472b44fd7ec59c6de3a
tomis9 <tomis9@buziaczek.pl>
2020-02-08 01:12:32 +0100

    minor fix in deploy

--- deploy.sh
+++ deploy.sh
@@ -6,30 +6,31 @@ dev='baseURL = ""'
 prod='baseURL = "http:\/\/tomis9\.com"'
 
 
-if [ $mod == "dev" ]
+if [ "$mod" == "dev" ]
 then
-    if [ $bs == "build" ]
+    if [ "$bs" == "build" ]
     then
         sed -i "1s/.*/$dev/" config.toml
         Rscript -e "blogdown::build_site()"
         sed -i "1s/.*/$prod/" config.toml
-    elif [ $bs == "serve" ]
+    elif [ "$bs" == "serve" ]
     then
         Rscript -e "blogdown::serve_site()"
     else
         echo "You must choose between build and serve"
     fi
-elif [ $mod == "prod" ]
+elif [ "$mod" == "prod" ]
 then
-    if [ $bs == "build" ]
+    if [ "$bs" == "build" ]
     then
         Rscript -e "blogdown::build_site()"
-    elif [ $bs == "serve" ]
+    elif [ "$bs" == "serve" ]
     then
         Rscript -e "blogdown::serve_site()"
     else
         echo "You must choose between build and serve"
     fi
 else
-    echo "You must choose between dev and prod."
+    echo "First argument:   [dev|prod]"
+    echo "Second argument:  [build|serve]"
 fi
